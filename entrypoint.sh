97fb5179b525add67ea705bbe500bd8aa7d09a23
Richard PERES <rperes@alpian.com>
2023-06-13 12:17:18 +0200

    Fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ POM_PATH="${1-.}"
 POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo "Founded version ${POM_VERSION}"
-echo "POM_VERSION=$POM_VERSION" >> $GITHUB_OUTPUT
+echo "version=$POM_VERSION" >> $GITHUB_OUTPUT
