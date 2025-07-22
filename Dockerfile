35c9687f96744134306380641bd0e2cfa0b28c25
Jens Papenhagen <jens.papenhagen@web.de>
2025-07-22 17:37:10 +0200

    adding new functions to the action

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM maven:3.9.3-eclipse-temurin-11-alpine
+FROM maven:3.9.11-eclipse-temurin-21-alpine
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,19 @@ MAVEN_ADDITIONAL_ARGS="${2-.}"
 MAVEN_CMD="mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout $MAVEN_ADDITIONAL_ARGS"
 
 echo "Running '$MAVEN_CMD' ..."
-POM_VERSION=$(eval $MAVEN_CMD)
+POM_VERSION=$(eval "$MAVEN_CMD")
 
 echo "Founded version ${POM_VERSION}"
-echo "version=$POM_VERSION" >> $GITHUB_OUTPUT
+echo "version=$POM_VERSION" >> "$GITHUB_OUTPUT"
+
+MAJOR_BASH="echo $POM_VERSION | cut -d. -f1"
+POM_MAJOR=$(eval "$MAJOR_BASH")
+echo "major=$POM_MAJOR" >> "$GITHUB_OUTPUT"
+
+MINOR_BASH="echo $POM_VERSION | cut -d. -f2"
+POM_MINOR=$(eval "$MINOR_BASH")
+echo "minor=$POM_MINOR" >> "$GITHUB_OUTPUT"
+
+PATCH_BASH="echo $POM_VERSION | cut -d. -f3"
+POM_PATCH=$(eval "$PATCH_BASH")
+echo "patch=$POM_PATCH" >> "$GITHUB_OUTPUT"
